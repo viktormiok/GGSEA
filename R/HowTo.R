5f0af462098304309464aeb677b8085388beafe4
Viktorian Miok <viktormiok@gmail.com>
2022-09-23 06:08:01 +0200

    version 1.0.0

---
+++ R/HowTo.R
@@ -0,0 +1,46 @@
+###------------------------------------------------------------------------- ###
+# This is our basic howto, keep it CLEAN and BASIC
+# only add STABLE and WORKING commands go here
+# 
+# Please comment every single line of code
+# 
+# for your working environment, you may copy this one but keep it lokal (not add to git)
+# Playground file shall be removed when obsolete
+###------------------------------------------------------------------------- ###
+
+
+###------------------------------------------------------------------------- ###
+# LOAD LIBRARYs
+# All needed Librarys go here
+# remove librarys if not needed
+# try to keep dependencies as short as possible
+
+
+###------------------------------------------------------------------------- ###
+# LOAD Example dataset
+# Link one ready processed (DESeq, Filering, ...) csv file here
+# use link from HMGU Box...
+
+
+###------------------------------------------------------------------------- ###
+# LOAD example Gene Set/ Pathway file 
+# Should use our internal function to generate the file in a format (list of lists)
+# we use internally
+
+
+###------------------------------------------------------------------------- ###
+# Perform Enrichment
+# Add function that performs or enrichment
+# one call for each internal enrichment algorithm (threshold, Distance Distribution,...)
+
+
+###------------------------------------------------------------------------- ###
+# Visualize Results
+# AAdd multiple(?) Visualization calls here
+
+
+###------------------------------------------------------------------------- ###
+# Additional Steps
+# Save, additional analyses, export to other tools,...
+
+
